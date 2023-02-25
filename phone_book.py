aec250cdb1c43483a970fe56bd81f85d7b2a303b
gonzalo lara <gonzaloflara@gmail.com>
2023-02-25 12:07:39 -0300

    DictReader terminado

--- phone_book.py
+++ phone_book.py
@@ -1,1 +1,10 @@
+import csv
 
+with open('data/phone_book.csv', mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file, skipinitialspace= True)
+    #archivo     importado. Lee como diccionari (open('data/phone_book.csv', mode='r'), skipintialspace=)
+    #next(csv_reader) #esto nos ayuda a skippear la primer fila(row) solo con listas
+
+    for row in csv_reader:
+        print(f'{row["first_name"]}: {row["phone_number"]}')
+        line_count += 1
